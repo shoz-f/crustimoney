dcfe23f95a059912a640fdc4b84bf94c91f27ff8
Arnout Roemers <github@company.romeinszoon.nl>
2016-04-26 21:54:04 +0200

    Simplify `advance`, fix `mk-value` and add `mk-flat`
    Seems to be working well now.

--- src/crustimoney/parse.clj
+++ src/crustimoney/parse.clj
@@ -1,103 +1,5 @@
-(ns crustimoney.parse)
-
-(def grammar {:expr   [:sum]
-              :sum    [:number :op :sum / :number]
-              :op     #"^\+|-"
-              :number #"^\d+"})
-
-;; ;; model
-
-;; {:grammar ...
-;;  :input ...
-;;  :steps
-;;  [{:rule [:sum]
-;;    :pos 0
-;;    :value nil}
-
-;;   {:rule :sum
-;;    :pos 0
-;;    :value nil}
-
-;;   {:rule [:number :op :sum / :number]
-;;    :pos 0
-;;    :value nil}
-
-;;   {:rule :number
-;;    :pos 0
-;;    :value nil}
-
-;;   {:rule #"^\d+"
-;;    :pos 0
-;;    :value nil}
-
-;;   {:rule :op
-;;    :pos 2
-;;    :value "42"}
-
-;;   {:rule #"^\+|-"
-;;    :pos 2
-;;    :value "42"}
-
-;;   {:rule :sum
-;;    :pos 3
-;;    :value "+"}
-
-;;   {:rule [:number :op :sum / :number]
-;;    :pos 3
-;;    :value "+"}
-
-;;   {:rule :number
-;;    :pos 3
-;;    :value "+"}
-
-;;   {:rule #"^\d+"
-;;    :pos 3
-;;    :value "+"}
-
-;;   {:rule :op
-;;    :pos 4
-;;    :value "2"}
-
-;;   {:rule #"^\+|-"
-;;    :pos 4
-;;    :value "2"}
-
-;;   {:rule :sum
-;;    :pos 5
-;;    :value "-"}
-
-;;   {:rule [:number]
-;;    :pos 5
-;;    :value "-"}
-
-;;   {:rule :number
-;;    :pos 5
-;;    :value "-"}
-
-;;   {:rule #"^\d+"
-;;    :pos 5
-;;    :value "-"}
-
-;;   {:rule nil
-;;    :pos 8
-;;    :value "100"}]}
-
-;; value
-
-;; 1. take "100", search for terminal (in this case #"^\d+") and add
-;;    to map with non-terminal above (in this case :number) as key.
-;; 2. take value of non-terminal from former step, so "-"
-;; 3. search for terminal (#"^\+|-") and use non-terminal above :op,
-;;    but as it has passed another non-terminal, first wrap current
-;;    value with that as key.
-
-;; {:sum {:number "42" :op "+" :sum {:number "2" :op "-" :sum {:number "100"}}}}
-
-;; recursion
-
-;; inside out: if a key holds a map, check whether that map holds a
-;; map (or seq) with the same key, if so, put those maps in a seq as
-;; the value.
+(ns crustimoney.parse
+  (:require [clojure.walk :refer (postwalk)]))
 
 (defn- regex? [v] (instance? java.util.regex.Pattern v))
 
@@ -107,26 +9,22 @@
    :steps [{:rule (get grammar start)
             :pos 0}]})
 
-(defn- forward [state value pos+]
-  (prn state)
+(defn- forward [state value]
   (let [steps (:steps state)
-        lstep (last steps)]
-    (loop [i (dec (count steps))]
+        last-index (dec (count steps))
+        last-step (nth steps last-index)]
+    (loop [i last-index]
       (if (< i 0)
-        {:done (update state :steps conj (assoc lstep
-                                                :rule nil
-                                                :value value
-                                                :pos (+ (:pos lstep) (count value))))
+        {:done (cond-> state value (assoc-in [:steps last-index :value] value))
          :success? true}
         (let [step (nth steps i)
               rule (:rule step)]
           (if (and (vector? rule) (not (contains? #{/ nil} (second rule))))
-            (-> (update step :rule (comp vec rest))
-                (->> (update state :steps assoc i))
-                (update :steps conj (assoc lstep
-                                           :rule (second rule)
-                                           :value value
-                                           :pos (+ (:pos lstep) pos+))))
+            (-> state
+                (update-in [:steps i :rule] (comp vec rest))
+                (cond-> value (assoc-in [:steps last-index :value] value))
+                (update :steps conj {:rule (second rule)
+                                     :pos (+ (:pos last-step) (count value))}))
             (recur (dec i))))))))
 
 (defn- backward [state]
@@ -140,8 +38,8 @@
           (if-let [alt (and (vector? rule) (seq (drop-while #(not= % /) rule)))]
             (-> state
                 (update :steps #(vec (take i %)))
-                (update :steps conj (assoc step :rule (vec alt)))
-                (forward (:value step) 0))
+                (update :steps conj {:rule (vec alt) :pos (:pos step)})
+                (forward nil))
             (recur (dec i))))))))
 
 (defn- advance [state]
@@ -150,38 +48,95 @@
         input (-> state :input)
         pos   (-> step :pos)]
     (cond (vector? rule)
-          (update state :steps conj (assoc step :rule (first rule)))
+          (update state :steps conj {:rule (first rule) :pos pos})
 
           (keyword? rule)
-          (update state :steps conj (assoc step :rule (get (:grammar state) rule)))
+          (update state :steps conj {:rule (get (:grammar state) rule) :pos pos})
 
           (regex? rule)
           (if-let [found (re-find rule (subs input pos))]
-            (forward state found (count found))
+            (forward state found)
             (backward state)))))
 
+
 (defn- mk-value [state]
-  (loop [result {}
-         value (-> state :steps last :value)
-         steps (-> state :steps reverse rest)
-         wrap? true]
+  (loop [value nil
+         steps (-> state :steps reverse)
+         result {}]
     (if-let [step (first steps)]
       (let [rule (:rule step)]
-        (cond (and (keyword? rule) wrap?)
-              (recur {rule result} value (rest steps) true)
-
-              (keyword? rule)
-              (recur (assoc result rule value) (:value step) (rest steps) true)
-
-              (regex? rule)
-              (recur result value (rest steps) false)
-
-              (vector? rule)
-              (recur result value (rest steps) wrap?)))
+        (if (keyword? rule)
+          (if value
+            (recur nil (rest steps) (assoc result rule value))
+            (recur nil (rest steps) {rule result}))
+          (recur (:value step) (rest steps) result)))
       result)))
+
+(defn- mk-flat [value]
+  (postwalk (fn [form]
+              (if (map? form)
+                (reduce-kv (fn [a k v]
+                             (if (and (map? v) (contains? v k))
+                               (let [vv (get v k)]
+                                 (if (vector? vv)
+                                   (assoc a k (vec (cons (dissoc v k) vv)))
+                                   (assoc a k [(dissoc v k) vv])))
+                               a))
+                           form
+                           form)
+                form))
+            value))
 
 (defn parse [grammar start input]
   (loop [s (mk-state grammar start input)]
     (if-let [d (:done s)]
-      (mk-value d)
+      (when (:success? s)
+        (-> (mk-value d) (mk-flat)))
       (recur (advance s)))))
+
+(comment
+  (def grammar {:expr   [:sum]
+                :sum    [:number :op :sum / :number]
+                :op     #"^\+|-"
+                :number #"^\d+"})
+
+
+  (mk-state grammar :expr "40+2")
+  ;;=>
+  {:grammar {:expr [:sum]
+             :sum  [:number :op :sum / :number]
+             :op   #"^\+|-" , :number #"^\d+"}
+   :input   "40+2"
+   :steps   [{:rule [:sum] , :pos 0}]}
+
+
+  (advance *1) ;; until done
+  ;;=>
+  {:grammar ...
+   :input   ...
+   :steps   [{:rule [:sum]           :pos 0}
+             {:rule :sum             :pos 0}
+             {:rule [:sum / :number] :pos 0} ;; the rule was [:number :op :sum / :number] in the
+                                             ;; beginning, but moving forward, next is called on it.
+             {:rule :number          :pos 0}
+             {:rule #"^\d+"          :pos 0 :value "40"}
+             {:rule :op              :pos 2}
+             {:rule #"^\+|-"         :pos 2 :value "+"}
+             {:rule :sum             :pos 3}
+             {:rule [:number]        :pos 3} ;; the rule was [:number :op :sum / :number] in the
+                                             ;; beginning, but backtracking set it to the
+                                             ;; alternative after the /.
+             {:rule :number          :pos 3}
+             {:rule #"^\d+"          :pos 3 :value "2"}]}
+
+
+  (mk-value *1)
+  ;;=>
+  {:sum {:sum {:number "2"}, :op "+", :number "40"}}
+
+
+  (mk-flat *1)
+  ;;=>
+  {:sum [{:op "+", :number "40"} {:number "2"}]}
+
+  )
